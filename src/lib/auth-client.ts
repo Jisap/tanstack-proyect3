65dc7b19244058f1d938af38165b7bc207882dac
Jorge Matos <jisapjisap@gmail.com>
2026-01-16 18:22:38 +0100

    feat: Initialize and export an authentication client using `better-auth/react`.

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,5 +1,5 @@
 import { createAuthClient } from 'better-auth/react'
 export const authClient = createAuthClient({
   //baseURL: 'http://localhost:3000',
-  baseURL: 'https://tanstack-proyect3-i3vpbff4m-jisaps-projects.vercel.app/'
+  baseURL: 'https://tanstack-proyect3-kgo3zuhmo-jisaps-projects.vercel.app'
 })
